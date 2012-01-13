e0eb97028e9c537345b1bc93492e6cdd6865c172
Ernesto Anaya <ernestex@gmail.com>
2012-01-13 05:29:38 -0500

    smile side2side

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html><body text=white bgcolor=black><h1>It works!</h1>
 <p>This is the default web page for this server.</p>
 <p>The web server software is running but no content has been added, yet.</p>
-<blockquote>Don't worry, be happy =)</blockquote>
+<blockquote>Don't worry, be happy <?=rand(0,1)?'=)':'(='?></blockquote>
 </body></html>
