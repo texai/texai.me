0d714aabc5c4f1264b7e347353bf8b7f32e7a19e
Ernesto Anaya <ernestex@gmail.com>
2012-01-24 07:24:41 -0500

    welcome message v0.1 and some Links

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<html><body text=white bgcolor=black><h1>It works!</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-<blockquote>Don't worry, be happy <?=rand(0,1)?'=)':'(='?></blockquote>
-</body></html>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="Author" content="texai" />
+    <title>texai's website</title>
+    <meta name="Description" content="website Ernesto Anaya Ruiz a.k.a. texai" />
+</head>
+<body>
+    <h1>Ernesto Anaya</h1>
+Hello, I'm Ernesto Anaya Ruiz some people know me as <a href="#">texai</a>.
+If you want you can reach me at:
+
+<ul>
+    <li><a href="http://twitter.com/texai" target="_blank">Twitter</a></li>
+    <li><a href="http://github.com/texai" target="_blank">GitHub</a></li>
+    <li><a href="http://stackoverflow.com/users/383403/texai" target="_blank">StackOverflow</a></li>
+    <li><a href="http://pe.linkedin.com/in/texai" target="_blank">Linked-In</a></li>
+</ul>
+
+
+</body>
+</html>
