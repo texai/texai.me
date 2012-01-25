77c3682797b4e0cdb06c1b8bca836bd3a4b69c7b
Ernesto Anaya <ernestex@gmail.com>
2012-01-25 16:40:43 -0500

    Adding basic style

--- index.php
+++ index.php
@@ -1,39 +1,35 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
+  <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="Author" content="texai" />
-    <title>texai's website</title>
-    <meta name="Description" content="website Ernesto Anaya Ruiz a.k.a. texai" />
-</head>
-<body>
-
-<h1>Ernesto Anaya personal site</h1>
-
-<h2>Quick Bio</h2>
-
-<p>Hello, my name is Ernesto Anaya Ruiz, some people knows me as <a href="http://blog.texai.me/">texai</a>,
-I live in <a href="http://en.wikipedia.org/wiki/Lima_Metropolitan_Area" target="_blank">Lima</a> for two years. I'm mainly a coder,
-also a <a href="http://perulinux.pe/capacitacion/php-zend-framework" target="_blank">teacher</a>.</p>
-
-<p>I love to learn new things, especially about web technologies,
-for this reason I have a <a href="http://blog.texai.me/">blog</a> where I will publishing some stuff
-time to time. If you wanna know more about me, you can <a href="http://twitter.com/texai" target="_blank">follow me</a>,
-take a look at my <a href="http://github.com/texai" target="_blank">source code</a>,
-my <a href="http://pe.linkedin.com/in/texai" target="_blank">professional profile</a> or
-my <a href="http://pe.linkedin.com/in/texai" target="_blank">cv</a>.</p>
-
-<p>If you want to reach me, please feel free to drop me an email to ernestex@gmail.com</p>
-
-<h2>More Links</h2>
-
-<ul>
-    <li><a href="http://stackoverflow.com/users/383403/texai" target="_blank">StackOverflow.</a></li>
-    <li><a href="http://youtube.com/user/texai" target="_blank">YouTube</a></li>
-    <li><a href="http://www.amazon.com/gp/registry/wishlist/204GD3AL0Y3Y7" target="_blank">Amazon WhishList</a></li>
-    <li><a href="http://www.hammerfest.es/user.html/141152" target="_blank">Hammerfest</a></li>
-</ul>
-
-
-</body>
+    <title>texai's personal homepage</title>
+    <meta name="Description" content="Personal homepage of Ernesto Anaya Ruiz a.k.a. texai" />
+    <link rel="stylesheet" href="style.css?v=1">
+  </head>
+  <body>
+    <div id="main">
+      <h1>Ernesto Anaya personal site</h1>
+      <h2>Quick Bio</h2>
+      <img src="photo.jpg" id="photo" align="right" alt="What are you looking for?"/>
+      <p>Hello, my name is Ernesto Anaya Ruiz, some people knows me as <a href="http://blog.texai.me/">texai</a>,
+      I live in <a href="http://en.wikipedia.org/wiki/Lima_Metropolitan_Area" target="_blank">Lima</a> for two years.
+      I'm mainly a coder, also a <a href="http://perulinux.pe/capacitacion/php-zend-framework" target="_blank">teacher</a>.</p>
+      <p>I love to learn new things, especially about web technologies, for this reason I have a
+      <a href="http://blog.texai.me/">blog</a> where I will publishing some stuff time to time. If you wanna know more about me,
+      you can <a href="http://twitter.com/texai" target="_blank">follow me</a>, take a look at my
+      <a href="http://github.com/texai" target="_blank">source code</a>, my
+      <a href="http://pe.linkedin.com/in/texai" target="_blank">professional profile</a> or my
+      <a href="CV-Ernesto_Anaya_Ruiz-dic2011.pdf" target="_blank">cv</a>.</p>
+      <p>If you want to reach me, please feel free to drop me an email to ernestex@gmail.com</p>
+      <h2>More Links</h2>
+      <ul>
+        <li><a href="http://stackoverflow.com/users/383403/texai" target="_blank">StackOverflow</a></li>
+        <li><a href="http://youtube.com/user/texai" target="_blank">YouTube</a></li>
+        <li><a href="http://www.amazon.com/gp/registry/wishlist/204GD3AL0Y3Y7" target="_blank">Amazon WhishList</a></li>
+        <li><a href="http://www.hammerfest.es/user.html/141152" target="_blank">Hammerfest</a></li>
+      </ul>
+      <hr/>
+    </div>
+  </body>
 </html>
